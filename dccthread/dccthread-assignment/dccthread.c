ebd0d6aac3d74ede9de3152eef9ea943128b36e8
Gabrimartins <gabrimartinsjuarez@gmail.com>
2023-04-16 19:19:28 -0300

    Init thread principal; includes e declarações incluídas; bugs removidos.

--- dccthread/dccthread-assignment/dccthread.c
+++ dccthread/dccthread-assignment/dccthread.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "dccthread.h"
 #include "ucontext.h"
+#include "dlist.h"
+
 
 typedef struct dccthread {
     int id;
@@ -8,24 +12,42 @@ typedef struct dccthread {
     ucontext_t contexto;
 } dccthread_t;
 
+struct dlist *lista_prontos;
+struct dlist *lista_espera;
+
+dccthread_t *gerente;
+dccthread_t *principal;
+
+
 void dccthread_init(void (*func)(int), int param){
     //inicializacao das estruturas internas da biblioteca
 
     //ga-
-    ready_list = dlist_create();
-	waiting_list = dlist_create();
+    lista_prontos = dlist_create();
+	lista_espera = dlist_create();
 
-	gerente = (dccthread_t *) malloc (sizeof(dccthread_t));
+	gerente = (dccthread_t *) malloc(sizeof(dccthread_t));
 	getcontext(&gerente->contexto);
 
     gerente->contexto.uc_link = NULL;
-	gerente->contexto.uc_stack.ss_sp = malloc (THREAD_STACK_SIZE);
+	gerente->contexto.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
 	gerente->contexto.uc_stack.ss_size = THREAD_STACK_SIZE;
 	gerente->contexto.uc_stack.ss_flags = 0;
 
     gerente->id = -1;
-    gerente->nome = "gerente";
+    *gerente->nome = "gerente";
 
+    //principal = dccthread_create("principal", func, param);
+
+    while(!dlist_empty(lista_prontos)){
+		dccthread_t *temp = (dccthread_t *) malloc(sizeof(dccthread_t));
+		temp = lista_prontos->head->data;
+
+		swapcontext(&gerente->contexto, &temp->contexto);
+		dlist_pop_left(lista_prontos);
+
+        free(temp);
+	}
     //-ga
 
     //criacao thread gerente
